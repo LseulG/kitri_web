e90194de12587fd559dcc53b0bad7cc38fefe08d
sseul <jjwisg@naver.com>
2019-02-08 16:35:41 +0900

    comm

--- boardmvc/src/com/kitri/action/Action.java
+++ boardmvc/src/com/kitri/action/Action.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Action {
-
+	// a
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 	
 }
